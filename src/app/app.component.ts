fb6a6bb9879049e68ea371cfe703de1802344d69
o-ian-carvalho <carvalhoian2509@gmail.com>
2024-11-23 20:49:29 -0300

    novo comit'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BotaoBlueComponent } from "./components/botao-blue/botao-blue.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BotaoBlueComponent],
+  imports: [RouterOutlet, BotaoBlueComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
