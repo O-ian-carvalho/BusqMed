6a03e67847ec52cb1a335a433ced86dbdba32849
Wallace Sander <90098336+1Sander@users.noreply.github.com>
2024-11-23 20:48:59 -0300

    initialFooterCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BotaoBlueComponent } from "./components/botao-blue/botao-blue.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BotaoBlueComponent],
+  imports: [RouterOutlet, BotaoBlueComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
