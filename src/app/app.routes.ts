158dd6c95c46084cbb4601d78df1ccb61ba2bbe6
o-ian-carvalho <carvalhoian2509@gmail.com>
2024-11-24 15:52:05 -0300

    responsividade

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MedicamentosComponent } from './pages/medicamentos/medicamentos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    { path: '', component: HomeComponent },
+    { path: 'medicamentos', component: MedicamentosComponent }
+];
